649718706741adfdebd6e4945ecd594f80c731e2
id2k1149 <id2k1149@gmail.com>
2022-11-08 12:11:17 -0500

    add extension ViewController: UITableViewDataSource

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -13,3 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //content for reusableCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reusableCell",
+                                                 for: indexPath)
+        // old way to use cell
+        cell.textLabel?.text = "Cell index: \(indexPath.row)"
+        return cell
+    }
+}
