aee60cc5e3c9cb27027a9a1d8cfb284841c92d57
id2k1149 <id2k1149@gmail.com>
2022-11-08 19:46:23 -0500

    edit Title for Screen

--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -22,6 +22,7 @@ class TrackListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "track", for: indexPath)
+        
         return cell
     }
 
