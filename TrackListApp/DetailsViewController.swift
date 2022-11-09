a9ccd944531f481bdee78477f7a95707bc009a06
id2k1149 <id2k1149@gmail.com>
2022-11-08 20:44:48 -0500

    add class DetailsViewController

---
+++ TrackListApp/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Max Franz Immelmann on 11/8/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
