dcfe499a06e2a901eef67603a8d0e31b6b374e48
id2k1149 <id2k1149@gmail.com>
2022-11-08 19:27:40 -0500

    add struct Track

---
+++ TrackListApp/Track.swift
@@ -0,0 +1,11 @@
+//
+//  Track.swift
+//  TrackListApp
+//
+//  Created by Max Franz Immelmann on 11/8/22.
+//
+
+struct Track {
+    let artist: String
+    let song: String
+}
--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -14,15 +14,10 @@ class TrackListViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 10
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "track", for: indexPath)
         return cell
