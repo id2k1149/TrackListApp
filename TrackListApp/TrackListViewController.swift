0d468c9234110470a0a84c2b7f7d946ffa7a88a6
id2k1149 <id2k1149@gmail.com>
2022-11-09 08:35:37 -0500

    add Edit button + move rows

--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -14,6 +14,9 @@ class TrackListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
+        
+        // navigation Edit button on left side
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
     // MARK: - Table view data source
@@ -42,6 +45,7 @@ class TrackListViewController: UITableViewController {
     
     
     // MARK: - UITableViewDelegate
+    
     /* method is too complicated in our case, we can use viewDidLoad with  tableView.rowHeight = 80
      
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -50,19 +54,40 @@ class TrackListViewController: UITableViewController {
     }
      */
     
+    // when we use view segue
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(indexPath.row)
         let track = trackList[indexPath.row]
+        
+        // send track to prepare segue
         performSegue(withIdentifier: "showDetails", sender: track)
     }
     
+    // remove delete icon
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
     
-
+    // remove movement of row
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    // move row + update tracklist
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
+    }
+    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailsVC = segue.destination as? TrackDetailsViewController else { return }
+        
+        // when we use button segue
 //        guard let indexPath = tableView.indexPathForSelectedRow else { return }
 //        detailsVC.track = trackList[indexPath.row]
+        
         detailsVC.track = sender as? Track
     }
 }
