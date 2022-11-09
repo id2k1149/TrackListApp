43e28d27f2b9e339f10da8de869821dfb5c856f0
id2k1149 <id2k1149@gmail.com>
2022-11-08 20:46:13 -0500

    rename TrackDetailsViewController

--- TrackListApp/TrackDetailsViewController.swift
+++ TrackListApp/TrackDetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class TrackDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
